79dff5820ea701c15d6fab1cd8030244691635fe
nuts-iko <nutsikosa@gmail.com>
2025-12-02 17:25:48 -0600

    fixed vendordeps

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -1,27 +1,30 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import frc.robot.Constants.DriveConstants;
 
 public class DriveBase extends SubsystemBase {
 
-    private final CANSparkMax leftFront = new CANSparkMax(DriveConstants.LEFT_FRONT_ID, MotorType.kBrushed);
-    private final CANSparkMax leftBack = new CANSparkMax(DriveConstants.LEFT_BACK_ID, MotorType.kBrushed);
-    private final CANSparkMax rightFront = new CANSparkMax(DriveConstants.RIGHT_FRONT_ID, MotorType.kBrushed);
-    private final CANSparkMax rightBack = new CANSparkMax(DriveConstants.RIGHT_BACK_ID, MotorType.kBrushed);
+    private final SparkMax leftFront  = new SparkMax(DriveConstants.LEFT_FRONT_ID, MotorType.kBrushless);
+    private final SparkMax leftBack   = new SparkMax(DriveConstants.LEFT_BACK_ID, MotorType.kBrushless);
+    private final SparkMax rightFront = new SparkMax(DriveConstants.RIGHT_FRONT_ID, MotorType.kBrushless);
+    private final SparkMax rightBack  = new SparkMax(DriveConstants.RIGHT_BACK_ID, MotorType.kBrushless);
 
     public DriveBase() {
-        leftBack.follow(leftFront);
-        rightBack.follow(rightFront);
-
+        // invert one side so both sides move forward correctly
         rightFront.setInverted(true);
+        rightBack.setInverted(true);
     }
 
     public void tankDrive(double leftSpeed, double rightSpeed) {
+        // manally set all motors
         leftFront.set(leftSpeed);
+        leftBack.set(leftSpeed);
+
         rightFront.set(rightSpeed);
+        rightBack.set(rightSpeed);
     }
 }
